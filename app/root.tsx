eae03311aef6d8c16257c2c1f2c5fce3d37aaa46
kle1sky <sergeyka100700@inbox.ru>
2024-05-30 20:27:20 +0300

    fix: favicon added

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import {
   isRouteErrorResponse,
   type ShouldRevalidateFunction,
 } from '@remix-run/react';
-import favicon from './assets/favicon.svg';
+import favicon from './assets/favicon.png';
 import resetStyles from './styles/reset.css?url';
 import appStyles from './styles/app.css?url';
 import {Layout} from '@/components/Layout';
